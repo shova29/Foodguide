72445876f5e0a033dff502ac3f53c9141d485a1e
Bibi Halima <halima2996@gmail.com>
2021-11-05 20:40:14 +0600

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	 <link href="CSS/bootstrap.min.css" rel="stylesheet">
+   <link rel="stylesheet" type="text/css" href="CSS/all.min.css">
+</head>
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+  <a class="navbar-brand" href="#" style="color: white;font-size: 70px;">About</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto" >
+      <li class="nav-item active" style="font:right;">
+        <a class="nav-link" href="homepage.php" style="color: white;font-size: 20px;margin-left: 100px;">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="foodgallery.php" style="color: white;font-size: 20px;">Food Items</a>
+      </li>
+       </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="review.php" style="color: white;font-size: 20px;">Review</a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="uploadimage.php" style="color: white;font-size: 20px;">Upload</a>
+      </li>
+       <li class="nav-item active">
+        <a class="nav-link" href="contact.php" style="color: white;font-size: 20px;">Contact <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item active">
+        <a class="nav-link" href="index.php" style="color: white;font-size: 20px;">Log Out<span class="sr-only">(current)</span></a>
+      </li>
+    </ul>
+   </div>
+</nav>
+</header>
+<body style="background-image: url(images/about.jpg);
+             background-size: cover; 
+             background-repeat:no-repeat;
+             opacity:0.8;">
+<p style="color:#00008B;font-size: 30px;font-family: serif;text-align:center;padding: 80px 50px; margin-left: 450px;">Any food lover can get a information of Food items through food guide.Also can give a review of food items.If anybody is interested,then they can also upload a photo.</p>
+<p clearfix  style="margin-left: 1050px;padding: 80px 80px;">&copy;Copy Right 2019 for foodguide.com
+<br>Designed & Developed by Bibi Halima Shova</p>
+</body>
+</html>
